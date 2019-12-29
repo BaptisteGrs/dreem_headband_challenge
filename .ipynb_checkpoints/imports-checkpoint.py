e8dba7351be14c6a6a40a32f473cc91162c33d6e
Baptiste <baptiste.gross@student.ecp.fr>
2019-12-29 19:39:59 +0100

    corrected paths

---
+++ .ipynb_checkpoints/imports-checkpoint.py
@@ -0,0 +1,19 @@
+# Baptiste GROSS
+# December 26th 2019
+# Dreem Sleep Stages Classification Challenge 
+
+# basic libraries
+
+import h5py
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import scipy.signal as sg
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn import metrics 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV
+
+# more exotic libraries
+from entropy import entropy
